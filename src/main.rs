4fed279062a6f897e757d90ccb385a24ff5102eb
andy <ckcy1@qq.com>
2022-10-11 15:14:58 +0800

    ...

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,16 @@ use std::io::Read;
 use std::fs::File;
 fn main() {
     // println!("Hello, world!");
-    fn read_username_from_file()->Result<String,io::Error>{
-        let f=File::open("hello.txt");
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let f = File::open("hello.txt");
         let mut f = match f {
-            Ok(file)=>file,
-            Err(e)=>{
-                return Err(e);
-            }
-            let mut s = string::new();
-            match f.read_to_string(&mut s) {
-                Ok(_)=>{Ok(s),
-                    Err(e)=>Err(e),
-        }
+            Ok(file) => file,
+            Err(e) => return Err(e),
+        };
+        let mut s = string::new();
+        match f.read_to_string(&mut s) {
+            Ok(_) => Ok(s),
+            Err(e) => Err(e),
+        };
     }
-        }
-            
+}
